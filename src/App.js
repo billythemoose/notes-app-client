77135a03a04fae6190de8a8c5af848f25868e33f
billythemoose <mdiegothomas@gmail.com>
2018-12-12 12:28:41 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    )
+  }
+}
+
+
+export default App;
+
+
+/*import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -26,3 +65,4 @@ class App extends Component {
 }
 
 export default App;
+*/
