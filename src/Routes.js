92b1f180a464d9b50c67e7f286fe59f5bc56602b
Diego <mdiegothomas@gmail.com>
2018-12-22 13:28:40 -0800

    Updated Routing
    Added authenticated / unauthenticated routing redirects
    
    Chagne

--- src/Routes.js
+++ src/Routes.js
@@ -5,15 +5,17 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
-        <AppliedRoute path="/login" exact component={Login} props={childProps} />
-        <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-        <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-        <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+        <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+        <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+        <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
         <AppliedRoute component={NotFound} />
     </Switch>;
